dac30b290142c09edb4528edf790d30ef37fb47c
abiy-local <as>
2014-03-26 23:34:27 -0700

    polling

--- factory/txn.js
+++ factory/txn.js
@@ -3,25 +3,30 @@ var q = require('Q');
 
 module.exports.checkForNewTx = function(request, response){
 	
-	var received, total_received, diff;
-
-
-	function q1 (){
+	var received = 0, 
+		total_received = 0, 
+		diff = 0;
+		
+	function q1 (then){
 		parse.getReceivedByAddress(request.params.address, function(statusCode, rawData){
 			if(statusCode == 200){
 				received = rawData;
-				console.log('q1 done');
+				if(then){
+					then();
+				}
 			} else {
 				console.log('ERROR: ' + statusCode);
 			}
 		});	
 	}	
 
-	function q2(){
+	function q2(then){
 		parse.getInfoByAddress(request.params.address, function(statusCode, rawData){
 			if(statusCode == 200){
 				total_received = rawData['total_received'];
-				console.log('q2 done');
+				if(then){
+					then();
+				}
 			} else {
 				console.log('ERROR: ' + statusCode);
 			}
@@ -29,16 +34,38 @@ module.exports.checkForNewTx = function(request, response){
 
 	}
 	
-	q.all([q1(), q2()])
-	.done(function(){
-		console.log("yay");
-	});
+	q1(q2);
+	q2(check);
 
-	function diff(v1, v2){
+	function difference(v1, v2){
 		return v2 - v1;
 	};
 
+	function check(){
+		diff = difference(received, total_received);
+		if(diff > 0){
+			sendPayment(diff);
+			setTimeout(function(){
+				q1(q2);
+				q2(check);
+			}, 6000);
+			out();
+		} else {
+			setTimeout(function(){
+				q1(q2);
+				q2(check);
+			}, 6000);
+			out();
+		}
+
+
+	}
+	
+	function sendPayment(payment){
+		console.log('SENDING...', payment);
+	}	
+
 	function out(){
-		response.send({total_received: total_received, received: received});
+		response.send({total_received: total_received, received: received, diff: diff});
 	}
 };
